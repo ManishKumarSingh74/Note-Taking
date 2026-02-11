aa735e01de8a240a7b796625d85ebcbf76efd03a
ManishKumarSingh74 <ms74882395@gmail.com>
2026-02-11 15:26:38 +0530

    Update backend URL to include API path

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backendUrl = axios.create({
-    baseURL:"https://note-taking-backend-hzac.onrender.com/"
+    baseURL:"https://note-taking-backend-hzac.onrender.com/api/note"
 })
 
 export default backendUrl
